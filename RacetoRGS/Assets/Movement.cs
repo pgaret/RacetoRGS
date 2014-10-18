8ac8b202c52903145f70abe53113f4e2999feff3
davidjrosenman <davidjrosenman@gmail.com>
2014-10-18 18:14:05 -0400

    Made bullets have a cooldown.

--- RacetoRGS/Assets/Movement.cs
+++ RacetoRGS/Assets/Movement.cs
@@ -7,6 +7,11 @@ public class Movement : MonoBehaviour {
 	public float speed = 10.0f;
 	//Bullet to be fired
 	public Transform bullet;
+	// Cooldown time for bullets
+	public float cooldown = 0.5f;
+	public float cooldownTimer = 0.5f;
+	// Duration of bullets
+	public float bulletLife = 0.1f;
 
 	int playerNumber;
 
@@ -21,6 +26,9 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (cooldownTimer > 0) {
+			cooldownTimer -= Time.deltaTime;
+		}
 		if (playerNumber == 1)
 		{
 			//Player1 movement based on input from WASD
@@ -29,7 +37,7 @@ public class Movement : MonoBehaviour {
 			if (Input.GetKey(KeyCode.S)) transform.Translate(Vector3.down*Time.deltaTime*speed);
 			if (Input.GetKey(KeyCode.D)) transform.Translate(Vector3.right*Time.deltaTime*speed);
 			//Player1 fires a bullet with the left shift key
-			if (Input.GetKey(KeyCode.LeftShift)) Instantiate(bullet, transform.position, Quaternion.identity);
+			if (Input.GetKey(KeyCode.LeftShift)) StartCoroutine(FireOneShot());
 		}
 		else
 		{
@@ -39,9 +47,22 @@ public class Movement : MonoBehaviour {
 			if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(Vector3.down*Time.deltaTime*speed);
 			if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(Vector3.right*Time.deltaTime*speed);
 			//Player2 fires a bullet with the right shift key
-			if (Input.GetKey(KeyCode.RightShift)) Instantiate(bullet, transform.position, Quaternion.identity);
+			if (Input.GetKey(KeyCode.RightShift)) StartCoroutine(FireOneShot());
 		}
 
 	
 	}
+
+	IEnumerator FireOneShot() {
+		// What happens when a shot is fired
+		if (cooldownTimer <= 0) {
+			//Debug.Log ("Can shoot!");
+			var bulletPos = transform.position;
+			var newBullet = Instantiate (bullet, bulletPos, Quaternion.identity);
+			cooldownTimer = cooldown;
+			yield return new WaitForSeconds (bulletLife);
+			Destroy (newBullet); // This still doesn't seem to work.
+		} //else
+			//Debug.Log ("Can't shoot!");
+	}	
 }
